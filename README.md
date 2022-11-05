5f40f742253bd79ba34c5daf56c6827e7b1e0e6a
Karasik Andrei <andrei.karasik@math.msu.ru>
2022-11-05 23:59:54 +0300

    Updated README and source file. Added comments and removed all the trash from the code

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # commands-to-serial-port-using-camera
 
+Program for sending commands to the control unit using the camera. [Link to BMSD brushed DC motor control unit.](https://electroprivod.ru/bdc-driver_bmsd.htm)
+Camera detects a circle with a division into black and white sectors, as in the screenshot below. We look at the intersection of the red vertical line and the blue horizontal.
+Determine the color of the incoming pixel and start the rotation by sending commands. Each time the black pixel occurs, the rotation speed is automatically switched.
+
+More details about the commands can be found in the BMSD passport.
+
 _**Tools/Packages**_: 
 Python 3.10, numpy, serial, cv2.
 
+_Screenshot of the program:_
 <a href="https://ibb.co/tmvMLJt"><img src="https://i.ibb.co/7YZJk15/screen.png" alt="screen" border="0"></a>
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-smth text
+Program for sending commands to the control unit using the camera
 """
 
 import time
@@ -9,8 +9,9 @@ import cv2
 import numpy as np
 import serial
 
-CFG_NAME = "bmsd.cfg"
+CFG_NAME = "bmsd.cfg"  # file with serial-port name
 
+# start commands
 G_CMD = [
     [0xE6, 0x00, 0xA5, 0x18, 0x4E],
     [0xE6, 0x00, 0xA6, 0x18, 0x1B],
@@ -21,16 +22,14 @@ G_CMD = [
 ]
 
 
-def bmsd_open_rs232(port):
+def bmsd_open_rs485(port):
     """open serial port"""
     print("opening " + port + "...")
 
-    hRS232 = None
-
     try:
         # open serial port and serial port parameters
-        hRS232 = serial.Serial(port=port, baudrate=9600, bytesize=serial.EIGHTBITS, stopbits=serial.STOPBITS_ONE,
-                               parity=serial.PARITY_NONE, timeout=1)
+        rs485 = serial.Serial(port=port, baudrate=9600, bytesize=serial.EIGHTBITS,
+                              stopbits=serial.STOPBITS_ONE, parity=serial.PARITY_NONE, timeout=1)
     except ValueError as err:
         print("ERROR: parameters are out of range")
         print(err)
@@ -41,13 +40,14 @@ def bmsd_open_rs232(port):
         sys.exit(-1)
     print("done\n")
 
-    return hRS232
+    return rs485
 
 
-def ComputeCRC8(inData, seed):
+def crc8(in_data, seed):
     """CRC algorithm"""
-    for i in range(8, 0, -1):
-        temp = ((seed ^ inData) & 0x01)
+    for j in range(8, 0, -1):
+        j += 1
+        temp = ((seed ^ in_data) & 0x01)
 
         if temp != 0:
             seed ^= 0x18
@@ -55,36 +55,38 @@ def ComputeCRC8(inData, seed):
             seed |= 0x80
         else:
             seed >>= 1
-        inData >>= 1
+        in_data >>= 1
 
     return seed
 
 
-def getSeed(line):
+def get_seed(line):
+    """helper function to determine the crc"""
     seed = 0
 
-    for i in line:
-        seed = ComputeCRC8(i, seed)
+    for k in line:
+        seed = crc8(k, seed)
 
     return seed
 
 
 def bmsd_gen_data():
-    for i in range(0, 250, 2):
-        line = [0xE6, 0x00, 0xA3, i]
-
-        line.append(getSeed(line[1:]))
+    """generation commands function"""
+    for k in range(0, 250, 2):
+        line = [0xE6, 0x00, 0xA3, k]
+        line.append(get_seed(line[1:]))
 
         G_CMD.insert(5, line)
 
 
-def bmsd_poll_data(hRS232, data):
+def bmsd_poll_data(rs_485, data):
+    """polling commands to the control unit"""
     print("polling...")
 
     # send command
     try:
-        hRS232.write(bytes(data))
-    except serial.SerialTimeoutException as err:
+        rs_485.write(bytes(data))
+    except serial.SerialTimeoutException:
         print("ERROR sending data")
 
     # wait timeout
@@ -100,155 +102,143 @@ def bmsd_poll_data(hRS232, data):
     print("\n")
 
 
-# greeting message
-print("bmsd: motor control app")
+if __name__ == '__main__':
+    # greeting message
+    print("bmsd: motor control app")
 
-# read configuration
-file = open(CFG_NAME, "r")
+    # read configuration
+    with open(CFG_NAME, "r") as file:
 
-if file.closed:
-    print("\nERROR opening configuration from " + CFG_NAME)
+        if file.closed:
+            print("\nERROR opening configuration from " + CFG_NAME)
+            sys.exit(-1)
 
-    sys.exit(-1)
+        using_port = file.read()
+        file.close()
 
-port = file.read()
+    # open serial port and check if succeeded
+    hRS232 = bmsd_open_rs485(using_port)
 
-file.close()
+    # generate commands
+    bmsd_gen_data()
 
-# open serial port and check if succeeded
-hRS232 = bmsd_open_rs232(port)
+    # iterator over a list with commands
+    i = 0
 
-# generate commands
-bmsd_gen_data()
+    # poll data
+    for i in range(0, 6):
+        bmsd_poll_data(hRS232, G_CMD[i])
 
-print(len(G_CMD))
+    TIMEENTER = None  # First entry time of black pixel
+    TIMECHANGE = None  # Time to change from black to white
+    TIMER_UP = None  # Timer checker
 
-# итератор для итерации по списку с командами
-i = 0
+    camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    # camera = cv2.VideoCapture(0)
 
-# poll data
-for i in range(0, 6):
-    bmsd_poll_data(hRS232, G_CMD[i])
+    while camera.isOpened():
+        WORKING = True
+        success, img = camera.read()
 
-# Первое время вхождения черного пикселя
-timeenter = None
+        img = cv2.resize(img, (img.shape[1] * 2, img.shape[0] * 2))
+        h = len(img)
+        w = len(img[0])
+        angle_rad = np.pi / 2
+        angle_deg = angle_rad * (180 / np.pi)
 
-# Время смены черного пикселя на белый
-timechange = None
+        x_up, y_up = w // 2, h // 4
+        x_down, y_down = w // 2, h - h // 4
+        x_right, y_right = w - w // 4, h // 2
+        x_left, y_left = w // 4, h // 2
 
-# Проверка на работу таймера
-timer_up = None
+        B_up, G_up, R_up = img[y_up][x_up]
+        avg_up = sum([R_up, G_up, B_up]) // 3
 
-camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-# camera = cv2.VideoCapture(0)
+        # while working:
+        if avg_up < 70:
+            avg_up = 0  # black
+            if TIMER_UP is not None and TIMEENTER is None:
+                TIMER_UP = True
+                timeenter = datetime.now()
 
-while camera.isOpened():
-    working = True
-    success, img = camera.read()
+                print(f'First entry time of black pixel: {TIMEENTER}')
 
-    img = cv2.resize(img, (img.shape[1] * 2, img.shape[0] * 2))
-    h = len(img)
-    w = len(img[0])
-    angle_rad = np.pi / 2
-    angle_deg = angle_rad * (180 / np.pi)
+        else:
+            # white
+            avg_up = 255
 
-    x_up, y_up = w // 2, h // 4
-    x_down, y_down = w // 2, h - h // 4
-    x_right, y_right = w - w // 4, h // 2
-    x_left, y_left = w // 4, h // 2
+            # We already have the first occurrence of black
+            if TIMER_UP and TIMEENTER is not None:
+                time_st = datetime.now() - TIMEENTER
 
-    B_up, G_up, R_up = img[y_up][x_up]
-    avg_up = sum([R_up, G_up, B_up]) // 3
+                milliseconds = (time_st.seconds * 10 ^ 6 + time_st.microseconds) / 1000
 
-    # while working:
-    if avg_up < 70:
-        avg_up = 0  # черный
-        if timer_up is not None and timeenter is None:
-            timer_up = True
-            timeenter = datetime.now()
+                time_current = round(milliseconds)
+                print(f'Time to change from black to white pixel: {time_current} milliseconds')
 
-            print(f'Первое время вхождения черного пикселя: {timeenter}')
+                v_angular = angle_rad / (time_current / 1000)
+                print(f'Omega = {round(v_angular, 4)} rad/sec')
 
-    else:
-        # белый
-        avg_up = 255
+                timechange = time_current
+                TIMER_UP = False
 
-        # у нас уже есть первое вхождение черного
-        if timer_up and timeenter is not None:
-            time_st = datetime.now() - timeenter
+                file = open('text.txt', 'a')
 
-            milliseconds = (time_st.seconds * 10 ^ 6 + time_st.microseconds) / 1000
+                # Prints first occurrence and angular speed
+                file.write(f'{TIMEENTER}\t{round(v_angular, 4)}\n')
+                file.close()
 
-            time_current = round(milliseconds)
-            print(f'Время смены черного на белый пиксель: {time_current} миллисекунд')
+                if i < len(G_CMD):
+                    # poll data
+                    bmsd_poll_data(hRS232, G_CMD[i])
+                    i += 1
+                else:
+                    camera.release()
 
-            v_angular = angle_rad / (time_current / 1000)
-            print(f'Omega = {round(v_angular, 4)} рад/с')
+                TIMEENTER = None
 
-            timechange = time_current
-            timer_up = False
+            elif TIMER_UP is None:
+                TIMER_UP = True
 
-            file = open('text.txt', 'a')
+        B_down, G_down, R_down = img[y_down][x_down]
+        avg_down = sum([R_down, G_down, B_down]) // 3
 
-            # печатает первое вхождение и скорость
-            file.write(f'{timeenter}\t{round(v_angular, 4)}\n')
-            file.close()
+        if avg_down < 60:
+            avg_down = 0
+        else:
+            avg_down = 255
 
-            if i < len(G_CMD):
-                # poll data
-                bmsd_poll_data(hRS232, G_CMD[i])
-                i += 1
-            else:
-                camera.release()
+        B_right, G_right, R_right = img[y_right][x_right]
+        avg_right = sum([R_right, G_right, B_right]) // 3
 
-            timeenter = None
+        if avg_right < 60:
+            avg_right = 0
+        else:
+            avg_right = 255
 
-        elif timer_up is None:
-            timer_up = True
+        B_left, G_left, R_left = img[y_left][x_left]
+        avg_left = sum([R_left, G_left, B_left]) // 3
 
-    B_down, G_down, R_down = img[y_down][x_down]
-    avg_down = sum([R_down, G_down, B_down]) // 3
+        if avg_left < 60:
+            avg_left = 0
+        else:
+            avg_left = 255
 
-    if avg_down < 60:
-        avg_down = 0
-    else:
-        avg_down = 255
+        cv2.line(img, (0, h // 2), (w, h // 2), (0, 0, 255), thickness=2)  # center
+        cv2.line(img, (w // 2, 0), (w // 2, h), (0, 0, 255), thickness=2)
+        cv2.line(img, (0, h // 4), (w, h // 4), (255, 0, 0), thickness=1)  # up
+        cv2.line(img, (w // 4, 0), (w // 4, h), (255, 0, 0), thickness=1)  # left
+        cv2.line(img, (w - w // 4, 0), (w - w // 4, h), (255, 0, 0), thickness=1)  # right
+        cv2.line(img, (0, h - h // 4), (w, h - h // 4), (255, 0, 0), thickness=1)  # down
 
-    # print('средняя яркость низ', avg_down)
+        cv2.imshow('circle_test', np.hstack([img]))
 
-    B_right, G_right, R_right = img[y_right][x_right]
-    avg_right = sum([R_right, G_right, B_right]) // 3
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            # working = False
+            break
 
-    if avg_right < 60:
-        avg_right = 0
-    else:
-        avg_right = 255
+    cv2.destroyAllWindows()
 
-    # print('средняя яркость прав', avg_right)
-
-    B_left, G_left, R_left = img[y_left][x_left]
-    avg_left = sum([R_left, G_left, B_left]) // 3
-
-    if avg_left < 60:
-        avg_left = 0
-    else:
-        avg_left = 255
-
-    cv2.line(img, (0, h // 2), (w, h // 2), (0, 0, 255), thickness=2)  # center
-    cv2.line(img, (w // 2, 0), (w // 2, h), (0, 0, 255), thickness=2)
-    cv2.line(img, (0, h // 4), (w, h // 4), (255, 0, 0), thickness=1)  # up
-    cv2.line(img, (w // 4, 0), (w // 4, h), (255, 0, 0), thickness=1)  # left
-    cv2.line(img, (w - w // 4, 0), (w - w // 4, h), (255, 0, 0), thickness=1)  # right
-    cv2.line(img, (0, h - h // 4), (w, h - h // 4), (255, 0, 0), thickness=1)  # down
-
-    cv2.imshow('circle_test', np.hstack([img]))
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        # working = False
-        break
-
-cv2.destroyAllWindows()
-
-hRS232.close()
-print("application terminated.")
-sys.exit(0)
+    hRS232.close()
+    print("application terminated.")
+    sys.exit(0)
